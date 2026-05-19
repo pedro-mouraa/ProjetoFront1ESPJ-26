6fc5f995603e01b8d373d936005c545db6244956
Pedro Moura <pedro030707@gmail.com>
2026-05-19 09:37:30 -0300

    feat: funcionalidades do menu hamburguer

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,16 @@ window.onload = function(){
 function trocar(cor){
     document.body.style.background=cor;
 }
+
+// MENU HAMBUERGUER 
+document.addEventListener("DOMContentLoaded", ()=>{
+    const menuIcone = document.getElementById("menu-icone");
+    const navMenu = document.querySelector("nav");
+
+    if(menuIcone && navMenu){
+        menuIcone.onclick=()=>{
+            navMenu.classList.toggle("active");
+            menuIcone.classList.toggle("open");
+        }
+    }
+});
